3dbf417420170223f6084e2fe5917c30a2e44be4
Kai Heath <kaiheath1277@gmail.com>
2023-04-04 21:50:39 -0600

    added websocket to update notes for both buddies

--- public/board.js
+++ public/board.js
@@ -1,5 +1,5 @@
-const username = localStorage.getItem('username');
-const user = async () => {
+
+const user = async (username) => {
     const fetchUrl = "/api/user/" + username;
     const response = await fetch (fetchUrl, {
     method: 'get', 
@@ -13,27 +13,35 @@ if (response?.status === 200) {
   }
 }
 
-
-notes = async () => {
-    currBuddy = localStorage.getItem('currBuddy');
-    const fetchUrl = "/api/user/board/" + [currBuddy, username];
-    const response = await fetch (fetchUrl, {
-    method: 'get',
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
+notes = async (username) => {
+  currBuddy = localStorage.getItem('currBuddy');
+  const fetchUrl = "/api/user/board/" + [currBuddy, username];
+  const response = await fetch (fetchUrl, {
+  method: 'get',
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
 });
 const body = await response?.json();
 if (response?.status === 200) {
-    return body;
-  }
-  return "error";
+  return body;
+}
+return "error";
 };
 
+class board {
+username;
+socket;
+
+constructor() {
+  this.username = localStorage.getItem("username");
+  this.configureWebSocket();
+  this.displayBoard();
+}
 
 
-async function displayBoard() {
-    const currUser = await user();
+async displayBoard() {
+    const currUser = await user(this.username);
     const currBuddy = currUser?.buddies[0];
     if (!currBuddy) {
       window.location.href = 'buddies.html';
@@ -41,54 +49,55 @@ async function displayBoard() {
     }
     localStorage.setItem('currBuddy', currBuddy);
     document.querySelector('#boardHeader').innerHTML = currBuddy + "'s and Your Board";
-    placeNotes();
+    this.placeNotes();
 }
 
-async function placeNotes() {
-    const noteStart1 = '<div draggable="true" class="box"> <button onclick="deleteNote(';
+  async placeNotes() {
+    const noteStart1 = '<div draggable="true" class="box"> <button onclick="Board.deleteNote(';
     const noteStart2 = ')" class ="btn btn-outline-danger" id="deleteButton">x</button><div>'
-    outputString = "";
-    noteBody = await notes();
-    notesText = noteBody?.notes;
-    length = notesText?.length;
-    outputString = "";
+    
+    let noteBody = await notes(this.username);
+    let notesText = noteBody?.notes;
+    let length = notesText?.length;
+    let outputString = "";
+    
 
-    for(i = 0; i < length; i++) {
-        note = await notesText[i];
+    for (let i = 0; i < length; i++) {
+        let note = notesText[i];
             outputString = outputString + noteStart1 + i + noteStart2 + note + '</div></div>';
     }
     
     document.querySelector('#buddyNotes').innerHTML = outputString;
 }
 
-async function deleteNote(i) {
+async deleteNote(i) {
     const fetchUrl = "/api/user/deleteNote";
     const response = await fetch (fetchUrl, {
     method: 'delete',
-    body: JSON.stringify({index : i, usernames : [localStorage.getItem("currBuddy"), username]}),
+    body: JSON.stringify({index : i, usernames : [localStorage.getItem("currBuddy"), this.username]}),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
 });
 
-    placeNotes();
+this.broadcastEvent([localStorage.getItem("currBuddy"), this.username])
 }
 
-async function addNote() {
-    text = document.querySelector('#newNoteText').value;
+async addNote() {
+    let text = document.querySelector('#newNoteText').value;
     document.querySelector('#newNoteText').value = '';
     const fetchUrl = "/api/user/addNote";
     const response = await fetch (fetchUrl, {
     method: 'post',
-    body: JSON.stringify({newNote : text, usernames : [localStorage.getItem("currBuddy"), username]}),
+    body: JSON.stringify({newNote : text, usernames : [localStorage.getItem("currBuddy"), this.username]}),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
 });
-    placeNotes();
+this.broadcastEvent([localStorage.getItem("currBuddy"), this.username])
 }
 
-async function addFunnyQuote() {
+async addFunnyQuote() {
   
   await fetch('https://api.chucknorris.io/jokes/random')
   .then((response) => response?.json())
@@ -104,6 +113,29 @@ async function addFunnyQuote() {
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-});
-    placeNotes();
+  });
+  this.broadcastEvent([localStorage.getItem("currBuddy"), this.username])
 }
+
+configureWebSocket() {
+  const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+  this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+  
+  this.socket.onmessage = async (event) => {
+    const msg = JSON.parse(await event.data.text());
+    const usernames = msg?.usernames;
+    if (usernames.includes(this.username)) {
+      this.placeNotes();
+    }
+  };
+}
+
+ broadcastEvent(usernames) {
+  const event = {
+    usernames : usernames
+  };
+  this.socket.send(JSON.stringify(event));
+}
+}
+
+
